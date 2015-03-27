446ef66a90b4e31208214c46ab5b4dbbb35e5b98
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-28 01:13:03 +0530

    Variables in Java
    Added directly structure of Intellij Idea

---
+++ src/Variables.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Jalpesh Vadgama on 28-03-2015.
+ */
+public class Variables {
+    public static  void main(String[] args){
+
+        // string variable
+        String name ="Jalpesh Vadgama";
+        System.out.println(name);
+
+        // integer variable
+        int number =10;
+        System.out.println(number);
+
+        //double variable
+        double doubleNumber =10.5;
+        System.out.println(doubleNumber);
+
+        //short varialble
+        short shortNumber =500;
+        System.out.println(shortNumber);
+
+        //float variable
+        float floatNumber =3.14F;
+        System.out.println(floatNumber);
+
+        //char variable
+        char firstCharacter ='j';
+        System.out.println(firstCharacter);
+
+        //boolean variable
+        boolean isComplete=true;
+        System.out.println(isComplete);
+
+        //byte varialble
+        byte byteNumber =127;
+        System.out.println(byteNumber);
+
+    }
+}
