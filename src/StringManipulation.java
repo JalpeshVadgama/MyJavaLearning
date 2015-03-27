be5a6b8546e793d47dcf62c91cff9c73e9c104df
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-28 01:24:51 +0530

    Added string manipulation code for the same.

--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -2,4 +2,19 @@
  * Created by Jalpesh Vadgama on 28-03-2015.
  */
 public class StringManipulation {
+
+    public static  void main(String[] args){
+
+        String firstName ="Jalpesh";
+        String lastName ="Vadgama";
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+        double salary=20000;
+        String nameSalary = fullName + salary;
+        System.out.println(nameSalary);
+
+    }
 }
+
