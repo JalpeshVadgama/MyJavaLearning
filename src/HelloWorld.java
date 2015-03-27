8638f711980f61ad00464db9cbfb403e31c45c4e
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-28 01:00:18 +0530

    Initial Commit with Hello World Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jalpesh Vadgama on 28-03-2015.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+
+}
