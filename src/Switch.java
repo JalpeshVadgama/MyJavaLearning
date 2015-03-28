0826d76724ff19c4cbdd825c8d118c7f944c2397
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-29 00:35:04 +0530

    Updated Switch Statement

--- src/Switch.java
+++ src/Switch.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 /**
  * Created by Jalpesh Vadgama on 29-03-2015.
  */
 public class Switch {
+
+    public static  void main(String[] args){
+
+        Scanner input =new Scanner(System.in);
+
+        System.out.println("Please enter command for machine:");
+        String text = input.nextLine();
+
+        switch (text){
+            case "Start":
+                System.out.println("Machine Started");
+                break;
+            case "Stop":
+                System.out.println("Machine not started");
+                break;
+            default:
+                System.out.println("It's not a valid command");
+                break;
+        }
+
+
+    }
+
 }
